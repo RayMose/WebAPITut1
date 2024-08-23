16dfe59d296bb9c9f50d5b9b5af51898c69136db
EAST-SOYA-DEV <codingwithsoya@gmail.com>
2024-08-23 23:34:53 +0300

    Inital commit

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using WebAPITut1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,19 @@ namespace WebAPITut1.Controllers
 {
     public class EmployeesController : ApiController
     {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBContext dBContext = new EmployeeDBContext())
+            {
+                return dBContext.Employees.ToList();
+            }
+        }
+        public Employee Get(int id)
+        {
+            using (EmployeeDBContext dbContext = new EmployeeDBContext())
+            {
+                return dbContext.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
     }
 }
