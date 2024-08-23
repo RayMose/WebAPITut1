fa9ed311b0110ab80b9001ca2760def73d6197fd
EAST-SOYA-DEV <codingwithsoya@gmail.com>
2024-08-23 23:28:02 +0300

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebAPITut1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPITut1.WebApiApplication" Language="C#" %>
